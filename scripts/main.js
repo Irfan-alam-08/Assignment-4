1b622c1b82e9fcd63237b9f8ec63319f296b7b71
IRFAN IFTI <160155423+Irfan-alam-08@users.noreply.github.com>
2026-05-09 03:34:09 +0600

    setup initial DOM selectors

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let totalJobs = document.getElementById("totalJobs");
+let interviewed = document.getElementById("totalInterview");
+let rejected = document.getElementById("totalRejected");
+let availableJobs = document.getElementById("availableJobs");
+let currentFilter = "all";
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+const allJobsSection = document.getElementById("jobCardContainer");
